13c2357e35cc703ce92fcce28e2784daaf301e27
emshomi <154547928+emshomi@users.noreply.github.com>
2026-06-03 13:20:52 -0500

    Add washington_sheriff_enabled configuration option

--- src/config.py
+++ src/config.py
@@ -105,6 +105,7 @@ class Settings(BaseSettings):
     scraper_ramsey_parcels_enabled: bool = Field(default=True)
     scraper_ramsey_tax_roll_enabled: bool = Field(default=True)
     scraper_anoka_sheriff_enabled: bool = Field(default=True)
+    scraper_washington_sheriff_enabled: bool = Field(default=True)
     scraper_ramsey_sheriff_enabled: bool = Field(default=True)
     scraper_mpls_vbr_enabled: bool = Field(default=True)
     scraper_saint_paul_vacant_enabled: bool = Field(default=True)
